fc2fe3602afbdcd6a42ac8b329cadabdf655e26d
unknown <zakihor3@gmail.com>
2020-01-18 18:16:05 +0200

    update Planetabhi v2.0

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('test');
+function hello() {
+  this.asd = 4;
+  console.log(this);
+}
+
+const Fun = function() {
+  return this;
+}
+
+const Arr = () => {
+  return this;
+}
+
+const person = {
+  name: 'Vladimen',
+  age: 25,
+  sayHello: hello,
+  helloF: function() {
+    console.log(this);
+  },
+  helloA: () => {
+    console.log(this);
+  }
+}
+
+console.log(Fun());
+console.log(Arr());
