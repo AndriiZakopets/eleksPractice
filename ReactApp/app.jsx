2b7aa1f973cbb6650509c713e0603aaf3dab1eb5
unknown <zakihor3@gmail.com>
2020-01-20 13:11:02 +0200

    reactApp

---
+++ ReactApp/app.jsx
@@ -0,0 +1,8 @@
+let container = 
+<div className="container">
+  <img src="https://html5book.ru/wp-content/uploads/2015/10/html5.png" />
+  <h1>React</h1>
+  <p>asdasdasdasdasds</p>
+</div>;
+
+ReactDOM.render(container, document.getElementById('root'));
